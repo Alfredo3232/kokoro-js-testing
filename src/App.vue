ef92296e8cd3bd562e30cc6df91331731a50aa65
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2025-01-16 16:44:31 -0800

    Add kokoro-js Setup page for uploading Lint and file changes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 style="color: whitesmoke">Kokoro JS</h1>
+
+  <form>
+    <div class="container">
+      <div class="form-group">
+        <label for="parser1">Insert Your Text Here</label>
+        <input type="text" id="parser1" />
+      </div>
+
+      <div class="form-group">
+        <label for="fileParser">Upload Your File Here</label>
+        <input type="file" id="fileParser" />
+      </div>
+    </div>
+  </form>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+  import { KokoroTTS } from "kokoro-js";
+  const model_id = "onnx-community/Kokoro-82M-ONNX";
+
+  // const tts = await KokoroTTS.from_pretrained(model_id, {
+  //   dtype: "q8", // Options: "fp32", "fp16", "q8", "q4", "q4f16"
+  // });
+
+  // const text = "Life is like a box of chocolates. You never know what you're gonna get.";
+  // const audio = await tts.generate(text, {
+  //   // Use `tts.list_voices()` to list all available voices
+  //   voice: "af_bella",
+  // });
+  // audio.save("audio.wav");
+
+  export default {};
+</script>
+
+<style>
+  body {
+    background-color: rgb(37, 37, 37);
+  }
+
+  .container {
+    width: 40vw;
+    height: 30vw;
+    background-color: whitesmoke;
+    border-radius: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+  }
+
+  .form-group {
+    flex: 1;
+    margin-bottom: 10px;
+  }
 </style>
